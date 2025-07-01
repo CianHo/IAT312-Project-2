f0ce5b534b0d25922227f622d26e2daf323f32d6
CianHo <cianho@me.com>
2025-06-30 23:52:44 -0700

    number lock version

---
+++ iat312-project-2/lock_highlight.gd
@@ -0,0 +1,7 @@
+extends "res://highlighter.gd"
+
+func _ready():
+	super()
+
+func _process(delta):
+	move(Vector2(8000.0,0))
--- iat312-project-2/number_lock.gd
+++ iat312-project-2/number_lock.gd
@@ -19,7 +19,7 @@ func incFirst():
 	elif first >=9:
 		first = 0
 	find_child("Label").set_text(str(first))
-	print("changing first")
+	check()
 
 func decFirst():
 	if first>=1:
@@ -27,6 +27,7 @@ func decFirst():
 	elif first <=0:
 		first = 9
 	find_child("Label").set_text(str(first))
+	check()
 
 
 func incSecond():
@@ -35,6 +36,7 @@ func incSecond():
 	elif second >=9:
 		second = 0
 	find_child("Label2").set_text(str(second))
+	check()
 
 func decSecond():
 	if second>=1:
@@ -42,6 +44,7 @@ func decSecond():
 	elif second <=0:
 		second = 9
 	find_child("Label2").set_text(str(second))
+	check()
 
 func incThird():
 	if third<=8:
@@ -49,6 +52,7 @@ func incThird():
 	elif third >=9:
 		third = 0
 	find_child("Label3").set_text(str(third))
+	check()
 
 func decThird():
 	if third>=1:
@@ -56,6 +60,7 @@ func decThird():
 	elif third <=0:
 		third = 9
 	find_child("Label3").set_text(str(third))
+	check()
 
 func incFourth():
 	if fourth<=8:
@@ -63,6 +68,7 @@ func incFourth():
 	elif fourth >=9:
 		fourth = 0
 	find_child("Label4").set_text(str(fourth))
+	check()
 
 func decFourth():
 	if fourth>=1:
@@ -70,6 +76,7 @@ func decFourth():
 	elif fourth <=0:
 		fourth = 9
 	find_child("Label4").set_text(str(fourth))
+	check()
 
 func incFifth():
 	if fifth<=8:
@@ -77,6 +84,7 @@ func incFifth():
 	elif fifth >=9:
 		fifth = 0
 	find_child("Label5").set_text(str(fifth))
+	check()
 
 func decFifth():
 	if fifth>=1:
@@ -84,3 +92,8 @@ func decFifth():
 	elif fifth <=0:
 		fifth = 9
 	find_child("Label5").set_text(str(fifth))
+	check()
+
+func check():
+	if first == 3 && second == 5 && third == 1 && fourth == 2 && fifth == 4:
+		print("opened")
